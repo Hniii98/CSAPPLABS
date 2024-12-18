b05680f2ac560297b044d5b5d4a729f905392bed
hniii98 <752708928@qq.com>
2024-12-18 11:01:22 +0900

    add:finish Lab1 DataLab's all puzzles

--- DataLab/bits.c
+++ DataLab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * <KunhuangZhu 752608928@qq.com>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -143,7 +143,9 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+/* transfer problem into exploit Xor gate by using and gate and not gate */
+	int result = ~(~(~x & y) & ~(x & ~y));
+	return result;
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +154,8 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+	/* implement tmin by shifting 1 with 31 times */
+  return 1 << 31;
 }
 //2
 /*
@@ -165,7 +166,11 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+	/* Use Tmax to construct 0x0 to detect and avoid 0xFFFFFFFF */
+//  return !(~(1 << 31) ^ x);
+	int result = ~(x + 1 + x); // construct 0xFFFFFFFF by using Tmax and reverse bits
+	result = (result | !(x + 1)); // exclude 0xFFFFFFFF by !(x + 1), because it has same property
+	return !result;
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +181,12 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+	/* detect symmertric difference by using mask 0xAAAAAAAA */
+	int mask = 0xAA;
+	int result; // should declare in front, otherwise dlc report comppilation error
+	mask = mask + (mask << 8) +(mask << 16) + (mask << 24); // construct mask
+	result  = !((x & mask) ^ mask);
+  return result;
 }
 /* 
  * negate - return -x 
@@ -186,7 +196,8 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+	/* reverse every bit then plus one */
+  return ~x + 1;
 }
 //3
 /* 
@@ -199,7 +210,12 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+	/* decompose inequlity into x-0x30 >=0 and 0x39-x >=0 and compute intersection */
+	int TMin = 1 << 31;
+	int lowerBound = !((x + ~0x30 + 1) & TMin);
+	int upperBound = !((~x + 1 + 0x39) & TMin);
+	int result = lowerBound & upperBound;
+  return result;
 }
 /* 
  * conditional - same as x ? y : z 
@@ -209,7 +225,11 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+	/* use bitsign 0xFFFFFFFF、0x00000000 implement output and block */
+	int logicSign = !!x; // any bit is one, logicsign equals True. Otherwise False. 
+	int bitSign  = ~logicSign + 1; // True turn into bit sign 0xFF..FF. False into 0x0.
+	int result = (bitSign & y) | (~bitSign & z); 
+  return result;
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -219,7 +239,10 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+    /* detect less or equal by sign bit */
+    int sign = 1 << 31;
+    int result = !((y +(~x + 1)) & sign); // fetch sign bit and calculate
+  return result;
 }
 //4
 /* 
@@ -231,8 +254,12 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+	/* use the property of difference between the opposite of zero , TMin , and the others. */
+	int bitUnion = (x | (~x + 1)); // x=0,-x=0; x=TMin,-x=TMin;x=the others, -x=-the others, compute their union by using | operator.
+	int result = (bitUnion >> 31) + 1; //  use arithmetic right shift to detect 0x00000000.
+  return result;
 }
+
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
  *  Examples: howManyBits(12) = 5
@@ -246,7 +273,25 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+	/* use bisection method to detect whether section exist at each time */
+	int signBit = x >> 31;
+	int exist16Bits, exist8Bits, exist4Bits, exist2Bits, exist1Bits; 
+	int result;
+	x = x ^ signBit; // use Xor operator to implement negation or maintain (x ^ 1 negation x ^ 0 maintain)
+
+	exist16Bits =  !!(x >> 16) << 4; // Bisection, if high 16BitExist, exist16Bits = 1 << 4 = 16, otherwise 0.
+	x = x >> exist16Bits; // if high 16bits exist, remove lower 16bits and keep detecting, otherwise don't shift.
+	exist8Bits = !!(x >> 8) << 3;
+	x = x >> exist8Bits;
+	exist4Bits = !!(x >> 4) << 2;
+	x = x >> exist4Bits;
+	exist2Bits = !!(x >> 2) << 1;
+	x = x >> exist2Bits;
+	exist1Bits = !!(x >> 1) << 0;
+	x = x >> exist1Bits;
+
+	result = x + exist1Bits + exist2Bits + exist4Bits + exist8Bits + exist16Bits + 1; // finally we need msb bit to represent sign, so +1.
+  return result;
 }
 //float
 /* 
@@ -261,7 +306,15 @@ int howManyBits(int x) {
  *   Rating: 4
  */
 unsigned floatScale2(unsigned uf) {
-  return 2;
+	/* implement expression by manipulating exponent bits and fraction bits */
+	unsigned expBits = (uf >> 23) & 0x000000FFu; 
+	unsigned fracBits = uf & 0x007FFFFFu;
+
+	if(expBits == 0xFF) return uf; // uf is NAN or infinite
+    else if(expBits == 0x00 && fracBits == 0x00) return uf; // when f = 0, 2 * 0 = 0, return uf
+	
+	if(expBits == 0x00) return uf + fracBits; // denormalize number, can only manipulate fraction to imlement x2.
+	else return uf + (1 << 23); // normalized number, due to the leading 1 in frac, can only manipulate exponent to implement x2 
 }
 /* 
  * floatFloat2Int - Return bit-level equivalent of expression (int) f
@@ -276,7 +329,39 @@ unsigned floatScale2(unsigned uf) {
  *   Rating: 4
  */
 int floatFloat2Int(unsigned uf) {
-  return 2;
+	/* transfer float by manipulate exponent and fraction */
+
+	unsigned expBits = (uf >> 23) & 0x000000FFu;
+	unsigned fracBits = uf & 0x007FFFFFu;
+	unsigned signBit = (uf >> 31) & 0x1u;
+	unsigned Bias = 127; // 2 ^ (k-1) - 1 = 12
+	unsigned normE; // normalized number's E, E = exp - Bias
+	unsigned normM; // normalized number's M, M = 1.frac
+					
+	if(expBits == 0xFF) return 0x80000000; // uf is NaN or infinity
+	else if(expBits == 0x00) return 0;   // denormalized, return 0
+
+	/* normalized number*/
+	// constrcut exp <= Bias - 1 to avoid representing negative number
+	if(expBits <= Bias - 1) return 0; // normalized, exp-Bias <= -1, number < 1, so return 0. 
+	normE = expBits - Bias;		     
+	normM = (1 << 23) + fracBits;   // add the leading 1.	
+	if(normE <= 23) 
+	{ 
+		if(!signBit)
+			return normM >> (23 - normE); // normlized norm <= 23, positive number 
+		else
+			return ~(normM >> (23 - normE)) + 1; // negative number
+	}
+	if(normE >= 24 && normE <= 31)
+	{
+		if(!signBit)
+			return normM << (normE - 23); // positive number
+		else
+			return ~(normM << (normE -23)) + 1;
+	}
+    // normE > 31, 32bit int can not represent anymore. includ TMIN
+	return 0x80000000u;
 }
 /* 
  * floatPower2 - Return bit-level equivalent of the expression 2.0^x
@@ -292,5 +377,26 @@ int floatFloat2Int(unsigned uf) {
  *   Rating: 4
  */
 unsigned floatPower2(int x) {
-    return 2;
+	/* transfer expression 2.0 ^ x by manipulating frac and exp */
+	int Bias = 127;
+	int posINF = 0x7F800000;
+	unsigned V = 0;
+	unsigned expBits;
+	unsigned fracBits; // all number all positive, don't need neg sign bit
+
+	//signle-prescision can represent  2.0^x ,which x locate in [-149, 127]
+
+	if(x >= -126 && x <= 127) // normal number
+	{
+		expBits = x + Bias; // all the result are postive, int/unsign can savely transfer.
+		return expBits << 23;
+	}
+	else if(x < -126 && x >= -149) // denorm number
+	{
+		fracBits = 0x1 << (x - (-149));
+		return fracBits;
+	}
+
+	if(x > 127) return posINF; // too large, larger than what normal number exp can represent 
+	else if(x < -149) return 0; // too small, smaller than what denorm can represent, frac can provide extral 23, which is LSB=1 
 }
